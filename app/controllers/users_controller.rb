fe5a5c27fbf8221c40735b2f893663e98b5e1964
SiriusLL <sirius.love.light@gmail.com>
2021-04-28 14:32:00 +0000

    user registration form, update user controller, fix database

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,6 +4,17 @@ class UsersController < ApplicationController
   end
 
   def create
+    user = User.new(user_params)
+      session[:user_id] = user.id
+    if user.save
+      redirect_to root_path
+    else
+      redirect_to signup_path
+    end
+  end
+
+  def user_params
+    params.require(:user).permit(:first_name, :email, :password, :password_confirmation)
   end
 
 end
